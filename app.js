a31cd3677d9fba29638b69450c3836f0c2ba3c6b
Gibril Touray <gibs2ray@gmail.com>
2026-06-19 15:07:42 +0000

    update response message to v2

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const http = require('http');
 const port = 3000;
 const server = http.createServer((req, res) => {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello from DevOps Lab!\n');
+  res.end('Hello from DevOps Lab -v2!\n');
 });
 server.listen(port, () => {
   console.log('App running on port ' + port);
